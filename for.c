6b1778e4d8d306a01b453bc64effea92a8d480e3
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-02-12 01:05:36 +0700

    12 Feb 2022

---
+++ for.c
@@ -0,0 +1,18 @@
+/* File         : for.c */
+/* Penulis      : Fauzyah Hadirahma, email faubelajar@gmail.com */
+/* Deskripsi    : print "Saya senang" sebanyak 3 kali */
+
+#include <stdio.h>
+int main () 
+{
+    /* KAMUS */
+    int i;
+    
+    /* ALGORITMA */
+    for (i=0; i<3; i++)
+        {
+            printf ("Saya senang belajar bahasa C \n");
+        }
+    
+    return 0;
+}
